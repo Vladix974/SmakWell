a12af9d13694c16f9cf046d054861b2bb2828897
Vladix974 <lutsenkovladislav974@gmail.com>
2023-04-02 11:21:18 +0300

    Add fragments and XML res

---
+++ app/src/main/java/com/example/smakwell/DescriptionFragment.kt
@@ -0,0 +1,6 @@
+package com.example.smakwell
+
+import androidx.fragment.app.Fragment
+
+class DescriptionFragment : Fragment(R.layout.fragmet_description) {
+}
---
+++ app/src/main/java/com/example/smakwell/RootFragment.kt
@@ -0,0 +1,7 @@
+package com.example.smakwell
+
+import androidx.fragment.app.Fragment
+
+class RootFragment :Fragment(R.layout.fragment_root){
+
+}
