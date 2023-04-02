c21e0210b0ac3c83c9c9e5b37b7a5ffcdd7fa636
Vladix974 <lutsenkovladislav974@gmail.com>
2023-04-02 11:33:03 +0300

    add navigation

--- app/src/main/java/com/example/smakwell/RootFragment.kt
+++ app/src/main/java/com/example/smakwell/RootFragment.kt
@@ -1,7 +1,15 @@
 package com.example.smakwell
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.smakwell.databinding.FragmentRootBinding
 
 class RootFragment :Fragment(R.layout.fragment_root){
+    private lateinit var binding: FragmentRootBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRootBinding.bind(view)
+    }
 
 }
